6c1b32de50c19d399eb2ef4f86af9f5df270a3da
Diego Arndt <diegoarndt@outlook.com>
2022-09-25 16:12:47 -0400

    Basic sum function developed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 try {
-    console.log(`Script.js has been sucessfully loaded.`);
+  console.log(`Script.js has been sucessfully loaded.`);
 } catch (error) {
-    console.error(`Script.js has not been loaded as expected. ${error}`);
+  console.error(`Script.js has not been loaded as expected. ${error}`);
+}
+
+function sum() {
+  document.getElementById('result').value =
+    +document.getElementById('first-number').value +
+    +document.getElementById('second-number').value;
 }
